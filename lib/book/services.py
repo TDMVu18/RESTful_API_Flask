1188f2ebb214f2f4a08c8a328a2cd0a2071a59e0
VuKer <74062885+TDMVu18@users.noreply.github.com>
2023-08-21 16:38:05 +0700

    update

--- lib/book/services.py
+++ lib/book/services.py
@@ -7,15 +7,24 @@ book_schema = BookSchema
 books_schema = BookSchema(many=True)
 
 def add_book_service():
-    name = request.json['name']
-    page_count = request.json['page_count']
-    author_id = request.json['author_id']
-    category_id = request.json['category_id']
-    try:
-        new_book = Books(name, page_count, author_id, category_id)
-        db.session.add(new_book)
-        db.session.commit()
-        return "Add success!"
-    except IndentationError:
-        db.session.rollback()
-        return "Can not add book!"
+    data = request.json
+    if data and ('name' in data) and ('page_count' in data) and ('author_id' in data) and ('category_id' in data):
+        name = data['name']
+        page_count = data['page_count']
+        author_id = data['author_id']
+        category_id = data['category_id']
+        try:
+            new_book = Books(name, page_count, author_id, category_id)
+            db.session.add(new_book)
+            db.session.commit()
+            return "Add success!"
+        except IndentationError:
+            db.session.rollback()
+            return "Can not add book!"
+    else:
+        return "Request error"
+
+def get_book_by_id_service(id):
+    book = Books.query.get(id)
+    if book:
+        
